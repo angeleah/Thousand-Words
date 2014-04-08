29c609f9e9b3f4b98734f0b8945044b69399b6eb
Angeleah Daidone <angeleah@angeleah.com>
2014-04-07 20:10:43 -0500

    Add CollectionView

---
+++ Thousand Words/TWPhotosCollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  TWPhotosCollectionViewController.h
+//  Thousand Words
+//
+//  Created by Angeleah Daidone on 4/7/14.
+//  Copyright (c) 2014 Angeleah Daidone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TWPhotosCollectionViewController : UICollectionViewController
+
+@end
---
+++ Thousand Words/TWPhotosCollectionViewController.m
@@ -0,0 +1,63 @@
+//
+//  TWPhotosCollectionViewController.m
+//  Thousand Words
+//
+//  Created by Angeleah Daidone on 4/7/14.
+//  Copyright (c) 2014 Angeleah Daidone. All rights reserved.
+//
+
+#import "TWPhotosCollectionViewController.h"
+
+@interface TWPhotosCollectionViewController ()
+
+@end
+
+@implementation TWPhotosCollectionViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - UICollectionViewDataSource
+
+-(UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Photo Cell";
+    
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:CellIdentifier forIndexPath:indexPath];
+    return cell;
+}
+
+-(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    return 5;
+}
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
